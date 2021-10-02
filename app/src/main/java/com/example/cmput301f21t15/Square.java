698650677cde4bb5707a3f3a688f0ba6f4450af1
Jason Hoang <jhoang2@ualberta.ca>
2021-10-02 16:22:28 -0600

    Adding Square class

---
+++ app/src/main/java/com/example/cmput301f21t15/Square.java
@@ -0,0 +1,10 @@
+package com.example.cmput301f21t15;
+
+public class Square extends Shape {
+	private Integer s;
+
+	public Square(Integer x, Integer y, Integer s) {
+		super(x,y);
+		this.s = s;
+	}
+}
