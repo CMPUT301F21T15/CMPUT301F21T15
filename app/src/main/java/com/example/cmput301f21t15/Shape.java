b3385798f3a03c0dce7639389d494b558cd1f2cf
TraianOglinzanu <oglinzan@ualberta.ca>
2021-10-02 14:34:14 -0600

    Lab4

---
+++ app/src/main/java/com/example/cmput301f21t15/Shape.java
@@ -0,0 +1,12 @@
+package com.example.cmput301f21t15;
+
+public abstract class Shape {
+
+    private Integer x;
+    private Integer y;
+
+    public Shape(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+}
