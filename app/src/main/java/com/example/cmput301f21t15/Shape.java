7a2ae18681e1164ff36a5bcae99243517d8757b3
Aaron Skiba <askiba@ualberta.ca>
2021-10-03 20:05:25 -0600

    Update Shape.java

--- app/src/main/java/com/example/cmput301f21t15/Shape.java
+++ app/src/main/java/com/example/cmput301f21t15/Shape.java
@@ -4,9 +4,11 @@ public abstract class Shape {
 
     private Integer x;
     private Integer y;
+    private String color;
 
-    public Shape(Integer x, Integer y) {
+    public Shape(Integer x, Integer y, String color) {
         this.x = x;
         this.y = y;
+        this.color = "blue";
     }
 }
