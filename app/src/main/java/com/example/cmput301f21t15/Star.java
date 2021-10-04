2a589a2ea30b40c2d80993cfdba3de85f57e1ea0
Aaron Skiba <askiba@ualberta.ca>
2021-10-03 21:14:08 -0600

    Update Star.java

--- app/src/main/java/com/example/cmput301f21t15/Star.java
+++ app/src/main/java/com/example/cmput301f21t15/Star.java
@@ -2,7 +2,7 @@ package com.example.cmput301f21t15;
 
 public class Star extends Shape {
 
-    public Star(Integer x, Integer ) {
+    public Star(Integer x, Integer y) {
         super(x,y);
     }
 }
